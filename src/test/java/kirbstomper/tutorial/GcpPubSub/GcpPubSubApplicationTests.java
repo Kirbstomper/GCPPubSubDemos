2d77fa5309c6fbaef22bb23a3aa82e128db62999
Chris Smith <wingedkraby@gmail.com>
2022-11-21 07:50:42 -0500

    Initial Commir

---
+++ src/test/java/kirbstomper/tutorial/GcpPubSub/GcpPubSubApplicationTests.java
@@ -0,0 +1,13 @@
+package kirbstomper.tutorial.GcpPubSub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpPubSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
